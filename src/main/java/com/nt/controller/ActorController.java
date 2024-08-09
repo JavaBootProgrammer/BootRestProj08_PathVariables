d20fa875ab1436fb51be83c6d5e0942e66dfbb3e
apasunoori <anand8533@gmail.com>
2024-08-09 13:56:51 -0400

    1

--- src/main/java/com/nt/controller/ActorController.java
+++ src/main/java/com/nt/controller/ActorController.java
@@ -10,8 +10,20 @@ public class ActorController {
 
 	@PostMapping("/report/{id}/{name}/{fee}")
 	public ResponseEntity<String> getActor(@PathVariable String id, @PathVariable String name,
-										   @PathVariable String fee) {
-		String message = "received path variables are : id->"+ id+ "; name->"+name+ "; fee->"+fee;
+			@PathVariable String fee) {
+		String message = "received path variables are : id->" + id + "; name->" + name + "; fee->" + fee;
+		System.out.println(message);
+		return new ResponseEntity<String>(message, HttpStatus.OK);
+
+	}
+
+	@PostMapping(value = {
+			"/reportOpt/{id}/{name}/{fee}",
+			"/reportOpt/{id}",
+			"/reportOpt/{id}/{name}" })
+	public ResponseEntity<String> getActorOptionalParams(@PathVariable String id,
+			@PathVariable(required = false) String name, @PathVariable(required = false) String fee) {
+		String message = "received path variables are : id->" + id + "; name->" + name + "; fee->" + fee;
 		System.out.println(message);
 		return new ResponseEntity<String>(message, HttpStatus.OK);
 
